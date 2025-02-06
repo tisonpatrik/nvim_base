f9a8c5a18689179a99e62bc6795a345563b366d0
Patrik <ptison@c2r7s1.42prague.com>
2025-02-06 09:45:06 +0100

    42 header was addded as plugin

---
+++ lua/plugins/42header.lua
@@ -0,0 +1,17 @@
+return {
+  "Diogo-ss/42-header.nvim",
+  cmd = { "Stdheader" },
+  keys = { "<F1>" },
+  opts = {
+    default_map = true, -- Default mapping <F1> in normal mode.
+    auto_update = true, -- Update header when saving.
+    user = "ptison", -- Your user.
+    mail = "ptison@student.42prague.com", -- Your mail.
+    -- add other options.
+  },
+  config = function(_, opts)
+    require("42header").setup(opts)
+  end,
+
+ }
+
