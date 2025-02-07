1e904aa99b6fc5e1fee4c0452731ec1e8427b5b5
Patrik <ptison@c2r7s1.42prague.com>
2025-02-07 19:53:15 +0100

    42 formatter was setuped as default for c and c++

--- lua/plugins/42_c_formatter.lua
+++ lua/plugins/42_c_formatter.lua
@@ -2,11 +2,21 @@ return {
   "Diogo-ss/42-C-Formatter.nvim",
   cmd = "CFormat42",
   config = function()
-    local formatter = require "42-formatter"
+    local formatter = require("42-formatter")
     formatter.setup({
       formatter = 'c_formatter_42',
       filetypes = { c = true, h = true, cpp = true, hpp = true },
     })
-    vim.api.nvim_set_keymap('n', '<F2>', ':CFormat42<CR>', { noremap = true, silent = true})
+
+    vim.api.nvim_set_keymap('n', '<F2>', ':CFormat42<CR>', { noremap = true, silent = true })
+
+    local formatgrp = vim.api.nvim_create_augroup("C_format_42", {})
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = {"*.c", "*.h", "*.cpp", "*.hpp"},
+      group = formatgrp,
+      command = "CFormat42",
+    })
   end
 }
+
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,14 +4,8 @@ return {
     local conform = require("conform")
     conform.setup({
       formatters_by_ft = {
-        c = { "clang-format" },
       },
       formatters = {
-        ["clang-format"] = {
-          prepend_args = {
-            [[-style={"IndentWidth": 4, "TabWidth": 4, "UseTab": "Always", "ColumnLimit": 80, "SeparateDefinitionBlocks": "Always", "AllowShortFunctionsOnASingleLine": "None", "KeepEmptyLines": {"AtEndOfFile": false, "AtStartOfBlock": false, "AtStartOfFile": false}, "MaxEmptyLinesToKeep": 1, "SpacesBeforeTrailingComments": 1, "TrimTrailingWhitespace": true, "ReflowComments": false, "BreakBeforeBraces": "Custom", "PackConstructorInitializers": "Never"}]]
-          },
-        },
       },
       format_on_save = {
         timeout_ms = 500,
